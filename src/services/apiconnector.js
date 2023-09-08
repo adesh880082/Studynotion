ea5f217f9509d4d021dd7c7c95d8e098f784ea01
Adesh Shrivastav <65299968+adesh880082@users.noreply.github.com>
2023-09-09 00:09:06 +0530

    undo last change

--- src/services/apiconnector.js
+++ src/services/apiconnector.js
@@ -9,8 +9,7 @@ export const apiConnector = (method, url, bodyData, headers, params) => {
         method: `${method}`,
         url: `${url}`,
         data: bodyData ? bodyData : null,
-        // headers: headers ? headers : null,
-        withCredentials: false,
+        headers: headers ? headers : null,
         params: params ? params : null,
 
     })
